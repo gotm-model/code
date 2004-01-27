6bb14efddcdbbe3ca3cb620dd2b44ca3ebfed493
lars <lars>
2004-01-27 08:31:00 +0000

    support for gen. stability function

--- src/turbulence/turbulence.F90
+++ src/turbulence/turbulence.F90
@@ -1,4 +1,4 @@
-!$Id: turbulence.F90,v 1.7 2003-03-28 09:20:35 kbk Exp $
+!$Id: turbulence.F90,v 1.8 2004-01-27 08:31:00 lars Exp $
 #include"cppdefs.h"
 !-----------------------------------------------------------------------
 !BOP
@@ -143,6 +143,7 @@
    integer, parameter, public :: tke_MY=3
 
 !  stability functions
+   integer, parameter        :: genASM=0
    integer, parameter        :: MellorYamada=1
    integer, parameter        :: KanClay=2
    integer, parameter        :: BurBaum=3
@@ -193,7 +194,10 @@
 
 !
 !  $Log: turbulence.F90,v $
-!  Revision 1.7  2003-03-28 09:20:35  kbk
+!  Revision 1.8  2004-01-27 08:31:00  lars
+!  support for gen. stability function
+!
+!  Revision 1.7  2003/03/28 09:20:35  kbk
 !  added new copyright to files
 !
 !  Revision 1.6  2003/03/28 08:20:01  kbk
@@ -2404,6 +2408,8 @@
    end if
 
    select case(stab_method)
+      case(genASM)
+         call cmue_gen(nlev)
       case(MellorYamada)
          call cmue_my(nlev)
       case(KanClay)
@@ -2612,109 +2618,110 @@
 !
 !-----------------------------------------------------------------------
 !BOC
-   NN=0. 
-   SS=0.
-   Ri=0.18  ! Initial guess for Rist
-      
-   do j=0,imax
-      Rii=Ri
-      ann=0.1
-      do i=0,imax
-         an(1)=ann
-         as(1)=an(1)/Rii
-         call stabilityfunctions(2,NN,SS,0)
-         fc=cmue1(1)*as(1)-cmue2(1)*an(1)-cm0**3
-         an(1)=ann+e
-         as(1)=an(1)/Rii
-         call stabilityfunctions(2,NN,SS,0)
-         fp=cmue1(1)*as(1)-cmue2(1)*an(1)-cm0**3
-         step=-fc/((fp-fc)/e)
-         ann=ann+0.5*step
+!!$   NN=0. 
+!!$   SS=0.
+!!$   Ri=0.18  ! Initial guess for Rist
+!!$      
+!!$   do j=0,imax
+!!$      Rii=Ri
+!!$      ann=0.1
+!!$      do i=0,imax
+!!$         an(1)=ann
+!!$         as(1)=an(1)/Rii
+!!$         call stabilityfunctions(2,NN,SS,0)
+!!$         fc=cmue1(1)*as(1)-cmue2(1)*an(1)-cm0**3
+!!$         an(1)=ann+e
+!!$         as(1)=an(1)/Rii
+!!$         call stabilityfunctions(2,NN,SS,0)
+!!$         fp=cmue1(1)*as(1)-cmue2(1)*an(1)-cm0**3
+!!$         step=-fc/((fp-fc)/e)
+!!$         ann=ann+0.5*step
+!!$
+!!$         if (abs(step).gt.100.) then
+!!$            STDERR '                                                 '
+!!$            STDERR 'Method for calculating the steady-state Richardson'
+!!$            STDERR 'number Rist does not converge.'
+!!$            STDERR 'Probably, the prescribed value for c3' 
+!!$            STDERR 'is outside the range of the chosen stability function.'
+!!$            STDERR 'Please change gotmturb.inp accordingly.'
+!!$            STDERR 'You have chosen the stability function no.',stab_method
+!!$            STDERR 'If the problem persists, then use another' 
+!!$            STDERR 'stability function.' 
+!!$            STDERR 'I will continue anyway. Good luck!' 
+!!$            STDERR '                                                 '
+!!$            converged=.false.
+!!$            goto 333
+!!$         endif
+!!$
+!!$         if (abs(step).lt.1.e-10) goto 111
+!!$      end do 
+!!$111   an(1)=ann
+!!$      as(1)=an(1)/Rii
+!!$      call stabilityfunctions(2,NN,SS,0)
+!!$      cc3=c2+(c1-c2)/Rii*cmue1(1)/cmue2(1)
+!!$      ffc=cc3-c3
+!!$
+!!$      Rii=Ri+ee
+!!$      ann=0.1
+!!$      do i=0,imax
+!!$         an(1)=ann
+!!$         as(1)=an(1)/Rii
+!!$         call stabilityfunctions(2,NN,SS,0)
+!!$         fc=cmue1(1)*as(1)-cmue2(1)*an(1)-cm0**3
+!!$         an(1)=ann+e
+!!$         as(1)=an(1)/Rii
+!!$         call stabilityfunctions(2,NN,SS,0)
+!!$         fp=cmue1(1)*as(1)-cmue2(1)*an(1)-cm0**3
+!!$         step=-fc/((fp-fc)/e)
+!!$         ann=ann+0.5*step
+!!$         if (abs(step).gt.100.) then
+!!$            STDERR 'Method for calculating the steady-state Richardson'
+!!$            STDERR 'number Rist does not converge.'
+!!$            STDERR 'Probably, the prescribed value for c3' 
+!!$            STDERR 'is outside the range of the chosen stability function.'
+!!$            STDERR 'Please change gotmturb.inp accordingly.'
+!!$            STDERR 'You have chosen the stability function no.',stab_method
+!!$            STDERR 'If the problem persists, then use another' 
+!!$            STDERR 'stability function.' 
+!!$            STDERR 'I will continue anyway. Good luck!' 
+!!$            STDERR '                                                 '
+!!$            converged=.false.
+!!$            goto 333
+!!$         endif
+!!$         if (abs(step).lt.1.e-10) goto 222
+!!$      end do 
+!!$222   an(1)=ann
+!!$      as(1)=an(1)/Rii
+!!$      call stabilityfunctions(2,NN,SS,0)
+!!$      cc3=c2+(c1-c2)/Rii*cmue1(1)/cmue2(1)
+!!$      ffp=cc3-c3
+!!$
+!!$      step=-ffc/((ffp-ffc)/ee)
+!!$      Ri=Ri+0.25*step
+!!$      if (abs(step).gt.100.) then
+!!$         STDERR '                                                 '
+!!$         STDERR 'Method for calculating the steady-state Richardson'
+!!$         STDERR 'number Rist does not converge.'
+!!$         STDERR 'Probably, the prescribed value for c3' 
+!!$         STDERR 'is outside the range of the chosen stability function.'
+!!$         STDERR 'Please change gotmturb.inp accordingly.'
+!!$         STDERR 'You have chosen the stability function no.',stab_method
+!!$         STDERR 'If the problem persists, then use another' 
+!!$         STDERR 'stability function.' 
+!!$         STDERR 'I will continue anyway. Good luck!'
+!!$         STDERR '                                                 '
+!!$         converged=.false.
+!!$         goto 333
+!!$    endif
+!!$      if (abs(step).lt.1.e-10) goto 333
+!!$   end do
+!!$   stop
+!!$
+!!$333   Rist=Ri
+!!$
+!!$   if (.not.converged) Rist=-999.
 
-         if (abs(step).gt.100.) then
-            STDERR '                                                 '
-            STDERR 'Method for calculating the steady-state Richardson'
-            STDERR 'number Rist does not converge.'
-            STDERR 'Probably, the prescribed value for c3' 
-            STDERR 'is outside the range of the chosen stability function.'
-            STDERR 'Please change gotmturb.inp accordingly.'
-            STDERR 'You have chosen the stability function no.',stab_method
-            STDERR 'If the problem persists, then use another' 
-            STDERR 'stability function.' 
-            STDERR 'I will continue anyway. Good luck!' 
-            STDERR '                                                 '
-            converged=.false.
-            goto 333
-         endif
-
-         if (abs(step).lt.1.e-10) goto 111
-      end do 
-111   an(1)=ann
-      as(1)=an(1)/Rii
-      call stabilityfunctions(2,NN,SS,0)
-      cc3=c2+(c1-c2)/Rii*cmue1(1)/cmue2(1)
-      ffc=cc3-c3
-
-      Rii=Ri+ee
-      ann=0.1
-      do i=0,imax
-         an(1)=ann
-         as(1)=an(1)/Rii
-         call stabilityfunctions(2,NN,SS,0)
-         fc=cmue1(1)*as(1)-cmue2(1)*an(1)-cm0**3
-         an(1)=ann+e
-         as(1)=an(1)/Rii
-         call stabilityfunctions(2,NN,SS,0)
-         fp=cmue1(1)*as(1)-cmue2(1)*an(1)-cm0**3
-         step=-fc/((fp-fc)/e)
-         ann=ann+0.5*step
-         if (abs(step).gt.100.) then
-            STDERR 'Method for calculating the steady-state Richardson'
-            STDERR 'number Rist does not converge.'
-            STDERR 'Probably, the prescribed value for c3' 
-            STDERR 'is outside the range of the chosen stability function.'
-            STDERR 'Please change gotmturb.inp accordingly.'
-            STDERR 'You have chosen the stability function no.',stab_method
-            STDERR 'If the problem persists, then use another' 
-            STDERR 'stability function.' 
-            STDERR 'I will continue anyway. Good luck!' 
-            STDERR '                                                 '
-            converged=.false.
-            goto 333
-         endif
-         if (abs(step).lt.1.e-10) goto 222
-      end do 
-222   an(1)=ann
-      as(1)=an(1)/Rii
-      call stabilityfunctions(2,NN,SS,0)
-      cc3=c2+(c1-c2)/Rii*cmue1(1)/cmue2(1)
-      ffp=cc3-c3
-
-      step=-ffc/((ffp-ffc)/ee)
-      Ri=Ri+0.25*step
-      if (abs(step).gt.100.) then
-         STDERR '                                                 '
-         STDERR 'Method for calculating the steady-state Richardson'
-         STDERR 'number Rist does not converge.'
-         STDERR 'Probably, the prescribed value for c3' 
-         STDERR 'is outside the range of the chosen stability function.'
-         STDERR 'Please change gotmturb.inp accordingly.'
-         STDERR 'You have chosen the stability function no.',stab_method
-         STDERR 'If the problem persists, then use another' 
-         STDERR 'stability function.' 
-         STDERR 'I will continue anyway. Good luck!'
-         STDERR '                                                 '
-         converged=.false.
-         goto 333
-    endif
-      if (abs(step).lt.1.e-10) goto 333
-   end do
-   stop
-
-333   Rist=Ri
-
-   if (.not.converged) Rist=-999.
-
+     Rist = -999.
    end function Rist
 !EOC
 
