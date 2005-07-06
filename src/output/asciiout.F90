5aa6f734209ac2e6232f1c94e5141814ca00cd8c
kbk <kbk>
2005-07-06 14:19:50 +0000

    added writing of obs. velocities

--- src/output/asciiout.F90
+++ src/output/asciiout.F90
@@ -1,32 +1,35 @@
-!$Id: asciiout.F90,v 1.4 2003-03-28 09:20:35 kbk Exp $
+!$Id: asciiout.F90,v 1.5 2005-07-06 14:19:50 kbk Exp $
 #include"cppdefs.h"
 !-----------------------------------------------------------------------
 !BOP
 !
 ! !MODULE: asciiout --- saving the results in ASCII
-! 
+!
 ! !INTERFACE:
    MODULE asciiout
 !
-! !DESCRIPTION: 
+! !DESCRIPTION:
 !  This module contains three subroutines for writing model output in ASCII format.
 !  The authors do not encourage using ASCII for output --- instead we recommend
-!  NetCDF. 
+!  NetCDF.
 !
 ! !USES:
    IMPLICIT NONE
-!   
+!
 !  Default all is private.
    private
 !
 ! !PUBLIC MEMBER FUNCTIONS:
    public init_ascii, do_ascii_out, close_ascii
 !
-! !REVISION HISTORY: 
+! !REVISION HISTORY:
 !  Original author(s): Karsten Bolding & Hans Burchard
 !
 !  $Log: asciiout.F90,v $
-!  Revision 1.4  2003-03-28 09:20:35  kbk
+!  Revision 1.5  2005-07-06 14:19:50  kbk
+!  added writing of obs. velocities
+!
+!  Revision 1.4  2003/03/28 09:20:35  kbk
 !  added new copyright to files
 !
 !  Revision 1.3  2003/03/10 08:53:05  gotm
@@ -82,15 +85,16 @@
 ! !IROUTINE: Save the model results to file
 !
 ! !INTERFACE:
-   subroutine do_ascii_out(nlev,timestr,variances,unit)
+   subroutine do_ascii_out(nlev,timestr,unit)
 !
 ! !DESCRIPTION:
 !  Writes all calculated data to an ASCII file.
 !
 ! !USES:
    use meanflow,     only: depth0,h,u,v,z,S,T,NN,buoy
-   use turbulence,   only: num,nuh,tke,eps,L,uu,vv,ww,tt,chi
-   use observations, only: tprof,sprof,epsprof
+   use turbulence,   only: num,nuh,tke,eps,L
+   use turbulence,   only: kb,epsb
+   use observations, only: tprof,sprof,uprof,vprof,epsprof
 !#ifdef SEDIMENT
 !   use sediment, only: ascii_sediment
 !#endif
@@ -103,7 +107,6 @@
 ! !INPUT PARAMETERS:
    integer, intent(in)                 :: nlev
    CHARACTER(len=*), intent(in)        :: timestr
-   logical, intent(in)                 :: variances
    integer, intent(in)                 :: unit
 !
 ! !REVISION HISTORY:
@@ -132,32 +135,25 @@
       write(unit,114) z(i),u(i),v(i),T(i),S(i),buoy(i)
    end do
 
-   write(unit,112) 'z','num','nuh','k','eps','L','NN'
+   write(unit,113) 'z','num','nuh','k','eps','L','kb','epsb'
    zz(1)=-depth0+h(1)
    do i=2,nlev
       zz(i)=zz(i-1)+h(i)
    end do
    do i=nlev-1,1,-1
-      write(unit,115) zz(i),num(i),nuh(i),tke(i),eps(i),L(i),NN(i)
+      write(unit,115) zz(i),num(i),nuh(i),tke(i),eps(i),L(i),kb(i),epsb(i)
    end do
 
-   if (variances) then
-      write(unit,112) 'z','uu','vv','ww','tt','chi'
-      do i=nlev-1,1,-1
-         write(unit,115) zz(i),uu(i),vv(i),ww(i),tt(i),chi(i)
-      end do
-   end if 
-
-   write(unit,113) 'z','Tobs','Sobs','epsobs'
+   write(unit,113) 'z','Tobs','Sobs','Uobs','Vobs','epsobs'
    do i=nlev,1,-1
-     write(unit,116) z(i),tprof(i),sprof(i),epsprof(i)
+     write(unit,116) z(i),tprof(i),sprof(i),uprof(i),vprof(i),epsprof(i)
    end do
 
 112 format(A9,6(1x,A10))
-113 format(A9,3(1x,A10))
+113 format(A9,8(1x,A10))
 114 format(F10.4,2(1x,E10.4E2),2(1x,F10.6),2(1x,E10.4E2))
-115 format(F10.4,6(1x,E10.4E2))
-116 format(F10.4,2(1x,F10.6),1(1x,E10.4E2))
+115 format(F10.4,7(1x,E10.4E2))
+116 format(F10.4,2(1x,F10.6),3(2x,E10.4E2))
 117 format(A9,4(1x,A10))
 
 !#ifdef SEDIMENT
@@ -207,4 +203,4 @@
 
 !-----------------------------------------------------------------------
 ! Copyright by the GOTM-team under the GNU Public License - www.gnu.org
-!----------------------------------------------------------------------- 
+!-----------------------------------------------------------------------
