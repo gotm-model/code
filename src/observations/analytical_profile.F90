1e4ac683071eb6bae2d90df4e725a29b5f2168e3
kbk <kbk>
2005-07-06 15:50:46 +0000

    added description - umlauf

--- src/observations/analytical_profile.F90
+++ src/observations/analytical_profile.F90
@@ -1,4 +1,4 @@
-!$Id: analytical_profile.F90,v 1.4 2003-03-28 09:20:35 kbk Exp $
+!$Id: analytical_profile.F90,v 1.5 2005-07-06 15:50:46 kbk Exp $
 #include "cppdefs.h"
 !-----------------------------------------------------------------------
 !BOP
@@ -9,8 +9,11 @@
    subroutine analytical_profile(nlev,z,z1,v1,z2,v2,prof)
 !
 ! !DESCRIPTION:
-!  This routine creates an analytical profile of a variable.
-!  It can be used to set up a simple two layer density structure.
+! This routine creates a vertical profile {\tt prof} with value
+! {\tt v1} in a surface layer down to depth {\tt z1} and a bottom
+! layer of value {\tt v2} reaching from depth {\tt z2} down to the bottom.
+! Both layers are connected by an intermediate layer reaching from {\tt z1}
+! to {\tt z2} with values linearly varying from {\tt v1} to {\tt v2}.
 !
 ! !USES:
    IMPLICIT NONE
@@ -27,46 +30,38 @@
 !  Original author(s): Karsten Bolding
 !
 !  $Log: analytical_profile.F90,v $
-!  Revision 1.4  2003-03-28 09:20:35  kbk
-!  added new copyright to files
+!  Revision 1.5  2005-07-06 15:50:46  kbk
+!  added description - umlauf
 !
-!  Revision 1.3  2003/03/28 09:02:09  kbk
-!  removed tabs
-!
-!  Revision 1.2  2003/03/10 08:51:57  gotm
-!  Improved documentation and cleaned up code
-!
-!  Revision 1.1.1.1  2001/02/12 15:55:58  gotm
-!  initial import into CVS
 !
 !EOP
 !
 ! !LOCAL VARIABLES:
    integer                   :: i
    REALTYPE                  :: alpha
-!   
+!
 !-----------------------------------------------------------------------
 !BOC
-   if (z2-z1 .gt. -1.e-15) then 
-         alpha = (v2-v1)/(z2-z1+2.e-15) 
+   if (z2-z1 .gt. -1.e-15) then
+         alpha = (v2-v1)/(z2-z1+2.e-15)
    else
       STDERR '**********************************************'
       STDERR '* Error detected by analytical_profile.F90:  *'
-      STDERR '*   z2 should be larger than z1.             *'            
+      STDERR '*   z2 should be larger than z1.             *'
       STDERR '*   Please edit obs.inp and restart GOTM.    *'
       STDERR '**********************************************'
       stop
-   end if 
+   end if
 
    do i=nlev,1,-1
       if(-1.*z(i) .le. z1) then
          prof(i) = v1
       end if
-      if (alpha.le.1.e15) then 
+      if (alpha.le.1.e15) then
          if(-1.*z(i) .gt. z1 .and. -1.*z(i) .le. z2) then
             prof(i) = v1 + alpha*(-1.*z(i)-z1)
          end if
-      end if 
+      end if
       if(-1.*z(i) .gt. z2) then
          prof(i) = v2
       end if
@@ -78,4 +73,4 @@
 
 !-----------------------------------------------------------------------
 ! Copyright by the GOTM-team under the GNU Public License - www.gnu.org
-!----------------------------------------------------------------------- 
+!-----------------------------------------------------------------------
