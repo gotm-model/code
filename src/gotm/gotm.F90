f0b7331dd187dd4eca80b72304b1147f1131d034
kbk <kbk>
2005-07-06 15:30:17 +0000

    added KPP, no bio, no sediment, updated documentation

--- src/gotm/gotm.F90
+++ src/gotm/gotm.F90
@@ -1,4 +1,4 @@
-!$Id: gotm.F90,v 1.16 2004-08-02 08:35:46 hb Exp $
+!$Id: gotm.F90,v 1.17 2005-07-06 15:30:17 kbk Exp $
 #include"cppdefs.h"
 !-----------------------------------------------------------------------
 !BOP
@@ -10,13 +10,13 @@
 !
 ! !DESCRIPTION:
 ! This is 'where it all happens'. This module provides the internal
-! routines {\tt init\_gotm()} to initialise the whole model and 
-! {\tt time\_loop()} to manage the time-stepping of all fields. These 
-! two routines in turn call more specialised routines e.g.\ of the 
+! routines {\tt init\_gotm()} to initialise the whole model and
+! {\tt time\_loop()} to manage the time-stepping of all fields. These
+! two routines in turn call more specialised routines e.g.\ of the
 ! {\tt meanflow} and {\tt turbulence} modules to delegate the job.
 !
-!  Here is also the place for a few words on Fortran `units' we used. 
-!  The method of Fotran units is quite rigid and also a bit dangerous,
+!  Here is also the place for a few words on FORTRAN `units' we used.
+!  The method of FORTRAN units is quite rigid and also a bit dangerous,
 !  but lacking a better alternative we adopted it here. This requires
 !  the definition of ranges of units for different purposes. In GOTM
 !  we strongly suggest to use units according to the following
@@ -27,59 +27,60 @@
 !     \item units 30-39 are reserved for the {\tt meanflow} module.
 !     \item units 40-49 are reserved for the {\tt turbulence} module.
 !     \item units 50-59 are reserved for the {\tt output} module.
-!     \item units 60-69 are reserved for the {\tt extra} modules 
-!           like those dealing with sediments or sea--grass.
-!     \item units 70- are \emph{not} reserved and can be used as you 
+!     \item units 60-69 are reserved for the {\tt extra} modules
+!           like those dealing with sediments or sea-grass.
+!     \item units 70- are \emph{not} reserved and can be used as you
 !           wish.
 !  \end{itemize}
 !
 ! !USES:
-   use airsea, only: init_air_sea,air_sea_interaction
-   use airsea, only: set_sst,integrated_fluxes
-   use airsea, only: calc_fluxes
-   use airsea, only: tx,ty,I_0,heat
    use meanflow
-   use turbulence
    use observations
    use output
    use time
-   use mtridiagonal
-   use eqstate
 
-#ifdef SEDIMENT
-   use sediment
-#endif
+   use airsea,      only: init_air_sea,air_sea_interaction
+   use airsea,      only: set_sst,integrated_fluxes
+   use airsea,      only: calc_fluxes
+   use airsea,      only: tx,ty,I_0,heat,p_e
+
+   use turbulence,  only: turb_method
+   use turbulence,  only: init_turbulence,do_turbulence
+   use turbulence,  only: num,nuh,nus
+   use turbulence,  only: const_num,const_nuh
+   use turbulence,  only: gamu,gamv,gamh,gams
+   use turbulence,  only: kappa
+
+   use kpp,         only: init_kpp,do_kpp
+
+   use mtridiagonal,only: init_tridiagonal
+   use eqstate,     only: init_eqstate
+
 #ifdef SEAGRASS
    use seagrass
 #endif
-#ifdef BIO
-   use bio
-   use bio_fluxes
-#endif
-!
+
    IMPLICIT NONE
    private
 !
 ! !PUBLIC MEMBER FUNCTIONS:
    public init_gotm, time_loop, clean_up
+
 !
 ! !DEFINED PARAMETERS:
    integer, parameter                  :: namlst=10
-#ifdef SEDIMENT
-   integer, parameter                  :: unit_sediment=61
-#endif
 #ifdef SEAGRASS
    integer, parameter                  :: unit_seagrass=62
-#endif
-#ifdef BIO
-   integer, parameter                  :: unit_bio=63
 #endif
 !
 ! !REVISION HISTORY:
 !  Original author(s): Karsten Bolding & Hans Burchard
 !
 !  $Log: gotm.F90,v $
-!  Revision 1.16  2004-08-02 08:35:46  hb
+!  Revision 1.17  2005-07-06 15:30:17  kbk
+!  added KPP, no bio, no sediment, updated documentation
+!
+!  Revision 1.16  2004/08/02 08:35:46  hb
 !  no need to pass time information
 !
 !  Revision 1.15  2004/07/29 17:36:36  hb
@@ -152,12 +153,17 @@
 !  This internal routine triggers the initialization of the model.
 !  The first section reads the namelists of {\tt gotmrun.inp} with
 !  the user specifications. Then, one by one each of the modules are
-!  initialised with help of more specialised routines like 
-!  {\tt init\_meanflow()} or {\tt init\_turbulence()} defined inside 
+!  initialised with help of more specialised routines like
+!  {\tt init\_meanflow()} or {\tt init\_turbulence()} defined inside
 !  their modules, respectively.
 !
+!  Note that the KPP-turbulence model requires not only a call to
+!  {\tt init\_kpp()} but before also a call to {\tt init\_turbulence()},
+!  since there some fields (fluxes, diffusivities, etc) are declared and
+!  the turbulence namelist is read.
+
 ! !USES:
-   IMPLICIT NONE
+  IMPLICIT NONE
 !
 ! !REVISION HISTORY:
 !  Original author(s): Karsten Bolding & Hans Burchard
@@ -170,8 +176,8 @@
    namelist /model_setup/ title,nlev,dt,cnpar,buoy_method
    namelist /station/     name,latitude,longitude,depth
    namelist /time/        timefmt,MaxN,start,stop
-   namelist /output/      out_fmt,out_dir,out_fn,nsave,variances, &
-                          diagnostics,mld_method,diff_k,Ri_crit,rad_corr
+   namelist /output/      out_fmt,out_dir,out_fn,nsave,diagnostics,     &
+                          mld_method,diff_k,Ri_crit,rad_corr
 !
 !-----------------------------------------------------------------------
 !BOC
@@ -181,17 +187,22 @@
 !  open the namelist file.
    LEVEL2 'reading model setup namelists..'
    open(namlst,file='gotmrun.inp',status='old',action='read',err=90)
+
    read(namlst,nml=model_setup,err=91)
-   timestep = dt ! timestep comes from the time module.
    read(namlst,nml=station,err=92)
    read(namlst,nml=time,err=93)
    read(namlst,nml=output,err=94)
-   depth0=depth
+
    LEVEL2 'done.'
 
+!  initialize a few things from  namelists
+   timestep   = dt
+   depth0     = depth
+
+!  write information for this run
    LEVEL2 trim(title)
    LEVEL2 'Using ',nlev,' layers to resolve a depth of',depth
-   LEVEL2 'The station ',trim(name),' is situated at (lat,long) ', &
+   LEVEL2 'The station ',trim(name),' is situated at (lat,long) ',      &
            latitude,longitude
    LEVEL2 trim(name)
 
@@ -203,30 +214,33 @@
 !  From here - each init_? is responsible for opening and closing the
 !  namlst - unit.
    call init_meanflow(namlst,'gotmmean.inp',nlev,latitude)
-   call init_tridiagonal(nlev) 
+   call init_tridiagonal(nlev)
    call updategrid(nlev,dt,zeta)
-   call init_turbulence(namlst,'gotmturb.inp',nlev)
-   call init_observations(namlst,'obs.inp',julianday,secondsofday, &
-                          depth,nlev,z,h)
+
+   call init_observations(namlst,'obs.inp',julianday,secondsofday,      &
+                          depth,nlev,z,h,gravity,rho_0)
+
+   call init_turbulence(namlst,'gotmturb.inp',nlev,depth,z0s,z0b)
+
+!  initalise mean fields
    s = sprof
    t = tprof
    u = uprof
    v = vprof
+
+!  initalise KPP model
+   if (turb_method.eq.99) then
+      call init_kpp(namlst,'kpp.inp',nlev,depth,h,gravity,rho_0)
+   endif
+
    call init_output(title,nlev,latitude,longitude)
    call init_air_sea(namlst,latitude,longitude)
 
-!  Initialise each of the extra features/modules
-#ifdef SEDIMENT
-   call init_sediment(namlst,'sediment.inp',unit_sediment,nlev, &
-                      gravity,rho_0)
-#endif
+!  initialise each of the extra features/modules
 #ifdef SEAGRASS
    call init_seagrass(namlst,'seagrass.inp',unit_seagrass,nlev,h)
 #endif
-#ifdef BIO
-   call init_bio(namlst,'bio.inp',unit_bio,nlev,h)
-   call init_bio_fluxes()
-#endif
+
    LEVEL2 'done.'
    STDERR LINE
 
@@ -242,10 +256,6 @@
    stop 'init_gotm'
 94 FATAL 'I could not read the "output" namelist'
    stop 'init_gotm'
-95 FATAL 'I could not read the "eqstate" namelist'
-   stop 'init_gotm'
-96 FATAL 'I could not read the "turbulence" namelist'
-   stop 'init_gotm'
    end subroutine init_gotm
 !EOC
 
@@ -259,23 +269,23 @@
 !
 ! !DESCRIPTION:
 ! This internal routine is the heart of the code. It contains
-! the main time--loop inside of which all routines required 
-! during the time step are called. The following main processes are 
+! the main time-loop inside of which all routines required
+! during the time step are called. The following main processes are
 ! successively triggered.
 ! \begin{enumerate}
 !  \item The model time is updated and the output is prepared.
-!  \item Air--sea interactions (flux, SST) are computed.
+!  \item Air-sea interactions (flux, SST) are computed.
 !  \item The time step is performed on the mean-flow equations
 !        (momentum, temperature).
-!  \item Some quantities related to shear and stratification are updated 
-!        (shear--number, buoyancy frequency, etc).
+!  \item Some quantities related to shear and stratification are updated
+!        (shear-number, buoyancy frequency, etc).
 !  \item Turbulence is updated depending on what turbulence closure
 !        model has been specified by the user.
 !  \item The results are written to the output files.
 ! \end{enumerate}
 !
-! Depending on macros set for the Fortran pre--processor, extra features
-! like the effects of sea--grass or sediments are considered in this routine 
+! Depending on macros set for the Fortran pre-processor, extra features
+! like the effects of sea-grass or sediments are considered in this routine
 ! (see \sect{sec:extra}).
 !
 ! !USES:
@@ -289,7 +299,10 @@
 !EOP
 !
 ! !LOCAL VARIABLES:
-   integer                   :: n
+   integer                   :: n,i
+
+   REALTYPE                  ::  tFlux,btFlux,sFlux,bsFlux
+   REALTYPE                  ::  tRad(0:nlev),bRad(0:nlev)
 !
 !-----------------------------------------------------------------------
 !BOC
@@ -297,6 +310,7 @@
 
    do n=MinN,MaxN
 
+!     prepare time and output
       call update_time(n)
       call prepare_output(n)
 
@@ -308,68 +322,69 @@
          call set_sst(T(nlev))
       end if
       call air_sea_interaction(julianday,secondsofday)
+
+!     reset some quantities
       tx = tx/rho_0
       ty = ty/rho_0
 
 !     meanflow integration starts
       call updategrid(nlev,dt,zeta)
       call coriolis(nlev,dt)
-      SS = 0.
-      call uequation(nlev,dt,cnpar,tx,num,PressMethod)
-      call vequation(nlev,dt,cnpar,ty,num,PressMethod)
+
+!     update velocity
+      call uequation(nlev,dt,cnpar,tx,num,gamu,PressMethod)
+      call vequation(nlev,dt,cnpar,ty,num,gamv,PressMethod)
       call extpressure(PressMethod,nlev)
-      if (int_press_method .ne. 0) call intpressure(nlev)
+      call intpressure(nlev)
       call friction(kappa,avmolu,tx,ty)
+
 #ifdef SEAGRASS
       call calc_seagrass(nlev,dt)
 #endif
-      if (s_prof_method .ne. 0.) call salinity(nlev,dt,cnpar,nuh)
-      if (t_prof_method .ne. 0.) &
-         call temperature(nlev,dt,cnpar,I_0,heat,nuh,rad)
-      call stratification(nlev,buoy_method,dt,cnpar,gravity,rho_0,nuh)
-#ifdef SEDIMENT
-      call do_sediment(nlev,dt)
-#endif
-#ifdef BIO
-      call do_bio_fluxes(julianday,secondsofday)
-      call do_bio(nlev,I_0,dt,h,t,nuh,rad,bioshade)
-#endif
+
+!     update temperature and salinity
+      if (s_prof_method .ne. 0) then
+         call salinity(nlev,dt,cnpar,nus,gams)
+      endif
+
+      if (t_prof_method .ne. 0) then
+         call temperature(nlev,dt,cnpar,I_0,heat,nuh,gamh,rad)
+      endif
+
+!     update shear and stratification
+      call shear(nlev,cnpar)
+      call stratification(nlev,buoy_method,dt,cnpar,nuh,gamh)
+
+
+!    compute turbulent mixing
       select case (turb_method)
-         case (0)
-            call convectiveadjustment(nlev,num,nuh,const_num,const_nuh,&
-                                      buoy_method,gravity,rho_0)
-         case (1)
-            STDERR '... turb_method=1 is not coded yet.'
-            STDERR 'Choose  turb_method=0 or turb_method=2.'
-            STDERR 'Program execution stopped ...'
-            stop 'time_loop'
-         case (2)
-            call production(nlev,alpha,num,nuh)
-            call stabilityfunctions(nlev,NN,SS,1)
-            call do_tke(nlev,dt,u_taus,u_taub,z0s,z0b,h,SS,NN,P,B)
-            call lengthscale(nlev,dt,z0b,z0s,u_taus,u_taub,depth,h,NN,P,B)
-            call turbulence_adv(nlev,dt,h)
-            call kolpran(nlev)
-         case default
+      case (0)
+!        do convective adjustment
+         call convectiveadjustment(nlev,num,nuh,const_num,const_nuh,    &
+                                   buoy_method,gravity,rho_0)
+      case (99)
+!        update KPP model
+         call convert_fluxes(nlev,gravity,cp,rho_0,heat,p_e,rad,T,S,    &
+                            tFlux,sFlux,btFlux,bsFlux,tRad,bRad)
+
+         call do_kpp(nlev,depth,h,rho,u,v,NN,NNT,NNS,SS,                &
+                     u_taus,u_taub,tFlux,btFlux,sFlux,bsFlux,           &
+                     tRad,bRad,cori)
+
+      case default
+!        update one-point models
+         call do_turbulence(nlev,dt,depth,u_taus,u_taub,z0s,z0b,h,      &
+                            NN,NNT,NNS,SS,SSU,SSV,xP)
       end select
 
-      call internal_wave(nlev,NN,SS)
-
+!     do the output
       call do_output(n,nlev)
 
       call integrated_fluxes(dt)
 
-!kbk - next version      call heat_content()
-
-!     write out variances
-      if(variances) then
-         call do_variances(nlev)
-      end if
-
-!     Diagnostic output
+!     diagnostic output
       if(diagnostics) then
-         call do_diagnostics(n,nlev,buoy_method,dt, &
-                             u_taus,u_taub,I_0,heat)
+         call do_diagnostics(n,nlev,buoy_method,dt,u_taus,u_taub,I_0,heat)
       end if
    end do
    STDERR LINE
@@ -387,7 +402,7 @@
    subroutine clean_up()
 !
 ! !DESCRIPTION:
-! This function is just a wrapper for the external routine 
+! This function is just a wrapper for the external routine
 ! {\tt close\_output()} discussed in \sect{sec:output}. All open files
 ! will be closed after this call.
 !
@@ -416,4 +431,4 @@
 
 !-----------------------------------------------------------------------
 ! Copyright by the GOTM-team under the GNU Public License - www.gnu.org
-!----------------------------------------------------------------------- 
+!-----------------------------------------------------------------------
