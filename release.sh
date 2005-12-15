b2496b26afeddbd550e3154a5b3a186d78f783b9
kbk <kbk>
2005-12-15 12:26:08 +0000

    stable should not be on a new branch

--- release.sh
+++ release.sh
@@ -45,7 +45,7 @@ if [ -d $release_dir/$release_name ] ; then
 fi
 
 if [ "$release_type" = "stable" ] ; then
-   cvs tag -b $TAG
+   cvs tag $TAG
    CVS2CL="cvs2cl -b -F $BRANCH --no-ancestors"
 fi
 
