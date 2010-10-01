9d1170ad36d3d041c5ee22cd71f237c9bb2463c2
jorn <jorn>
2010-10-01 12:36:32 +0000

    removed superfluous spaces

--- gui.py/gui_util.F90
+++ gui.py/gui_util.F90
@@ -1,20 +1,20 @@
-!$Id: gui_util.F90,v 1.2 2007-01-19 09:40:25 jorn Exp $
+!$Id: gui_util.F90,v 1.3 2010-10-01 12:36:32 jorn Exp $
 
 #include"cppdefs.h"
 
 module gui_util
     implicit none
-    
+
     logical, private :: redirected = .false.
 
 contains
 
     subroutine redirectoutput(outpath,errpath)
         implicit none
-        
+
         character(len=255), intent(in) :: outpath
         character(len=255), intent(in) :: errpath
-        
+
         call resetoutput()
         open(unit=stdout,file=outpath)
         open(unit=stderr,file=errpath)
@@ -23,20 +23,20 @@ contains
 
     subroutine resetoutput()
         implicit none
-    
+
         if (redirected) then
             close(unit=stdout)
             close(unit=stderr)
             redirected = .false.
         end if
     end subroutine resetoutput
-    
+
     subroutine getversion(versionstring)
         implicit none
-        
+
         character(len=255), intent(out) :: versionstring
-        
+
         versionstring = RELEASE
     end subroutine getversion
-    
+
 end module gui_util
