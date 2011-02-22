11d03ef97b32ee9019705e00028e4a5805a8afdd
jorn <jorn>
2011-02-22 10:46:05 +0000

    added coordinate reassignment for velocity points in MOM4

--- gui.py/xmlplot/data/netcdf.py
+++ gui.py/xmlplot/data/netcdf.py
@@ -1648,5 +1648,14 @@ class NetCDFStore_MOM4(NetCDFStore):
                 self.reassigneddims['xt_ocean' ] = 'geolon_t'
                 self.reassigneddims['yt_ocean'] = 'geolat_t'
 
+        if ('xu_ocean'  in ncdims and 'yu_ocean' in ncdims and
+            'geolon_c' in ncvars and 'geolat_c' in ncvars):
+            lon = numpy.ma.compressed(getNcData(ncvars['geolon_c']))
+
+            # Only reassign dimension if alternative coordinate values have a meaningful value.
+            if lon.shape[0]>0 and (lon!=lon[0]).any():
+                self.reassigneddims['xu_ocean' ] = 'geolon_c'
+                self.reassigneddims['yu_ocean'] = 'geolat_c'
+
 NetCDFStore.registerConvention(NetCDFStore_GOTM)
 NetCDFStore.registerConvention(NetCDFStore_MOM4)
