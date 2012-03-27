2235fac824b545f9ed6456c9a4fa0993f77d8e90
jornbr <jorn@bolding-burchard.com>
2012-03-27 09:18:54 +0100

    refactored qt_compat; prefer PyQt4 over PySide

--- gui.py/xmlstore/qt_compat.py
+++ gui.py/xmlstore/qt_compat.py
@@ -1,60 +1,48 @@
-#!/usr/bin/env python
-
-from __future__ import with_statement
-from __future__ import division
-
-_TRY_PYSIDE = True
-
-try:
-	if not _TRY_PYSIDE:
-		raise ImportError()
-	import PySide.QtCore as _QtCore
-	QtCore = _QtCore
-	USES_PYSIDE = True
-except ImportError:
+def importPyQt4():
 	import sip
-	sip.setapi('QString', 2)
+	sip.setapi('QString',  2)
 	sip.setapi('QVariant', 2)
 	import PyQt4.QtCore as _QtCore
-	QtCore = _QtCore
-	USES_PYSIDE = False
+	return _QtCore
+    
+def importPySide():
+	import PySide.QtCore as _QtCore
+	return _QtCore
 
+preference = ('PyQt4',importPyQt4),('PySide',importPySide)
 
-def _pyside_import_module(moduleName):
-	pyside = __import__('PySide', globals(), locals(), [moduleName], -1)
-	return getattr(pyside, moduleName)
+qt4_backend = None
+for name,importFunction in preference:
+    try:
+        QtCore = importFunction()
+    except ImportError:
+        continue
+    qt4_backend = name
+    break
+else:
+    raise Exception('Unable to import PyQt4 or PySide. Please install one of these packages first.')
 
+def importModule(moduleName):
+	qt4 = __import__(qt4_backend, globals(), locals(), [moduleName], -1)
+	return getattr(qt4, moduleName)
 
-def _pyqt4_import_module(moduleName):
-	pyside = __import__('PyQt4', globals(), locals(), [moduleName], -1)
-	return getattr(pyside, moduleName)
-
-
-if USES_PYSIDE:
+# Store properties describing backend.
+# Create additional methods in QtCore module where needed.
+if qt4_backend=='PySide':
 	import PySide
-	import_module = _pyside_import_module
-	
 	mpl_qt4_backend = 'PySide'
-	qt4_backend = 'PySide'
 	qt4_backend_version = PySide.__version__
 else:
-	import_module = _pyqt4_import_module
-
-	QtCore.Signal = QtCore.pyqtSignal
-	QtCore.Slot = QtCore.pyqtSlot
-	QtCore.Property = QtCore.pyqtProperty
-
 	mpl_qt4_backend = 'PyQt4'
-	qt4_backend = 'PyQt4'
 	qt4_backend_version = QtCore.PYQT_VERSION_STR
 
-QtGui = import_module('QtGui')
+	QtCore.Signal   = QtCore.pyqtSignal
+	QtCore.Slot     = QtCore.pyqtSlot
+	QtCore.Property = QtCore.pyqtProperty
 
-if USES_PYSIDE:
+# Import QtGui module and create additional methods where needed.
+QtGui = importModule('QtGui')
+if qt4_backend=='PySide':
 	QtGui.QFileDialog.getOpenFileNamesAndFilter = QtGui.QFileDialog.getOpenFileNames
 	QtGui.QFileDialog.getOpenFileNameAndFilter = QtGui.QFileDialog.getOpenFileName
 	QtGui.QFileDialog.getSaveFileNameAndFilter = QtGui.QFileDialog.getSaveFileName
-
-if __name__ == "__main__":
-	pass
-
