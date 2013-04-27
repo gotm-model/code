34f4b1b79e7bb33d42d9e81fc92267ccf417f46c
Jorn Bruggeman <jorn@bolding-burchard.com>
2013-04-27 13:09:28 +0100

    removed non-applicable documentation from water_balance

--- src/meanflow/water_balance.F90
+++ src/meanflow/water_balance.F90
@@ -8,52 +8,6 @@
    subroutine water_balance()
 !
 ! !DESCRIPTION:
-!  This subroutine updates the bottom roughness
-!  \begin{equation}
-!    \label{Defz0b}
-!    z_0^b = 0.1 \frac{\nu}{u_*^b} + 0.03 h_0^b + z_a \point
-!  \end{equation}
-!  The first term on the right hand side of \eq{Defz0b} represents
-!  the limit for hydraulically smooth surfaces, the second term the limit
-!  for completely rough surfaces. Note that the third term, $z_a$,
-!  is the contribution of suspended sediments to the
-!  roughness length, see \cite{SmithMcLean77}. It is updated during calls
-!  to the sediment-routines.
-!
-! The law-of-the-wall relations are used to compute the friction velocity
-! \begin{equation}
-!  \label{uStar}
-!   u_*^b = r \sqrt{U_1^2 + V_1^2}
-!   \comma
-! \end{equation}
-! where $U_1$ and $V_1$ are the components of the mean velocity
-! at the center of the lowest cell.
-! We used the abbreviation
-!  \begin{equation}
-!    \label{rParam}
-!    r=\frac{\kappa}{\ln \left( \frac{0.5h_1+z_0^b}{z^b_0} \right)}
-!    \comma
-!  \end{equation}
-!  where $\kappa$ is the von K{\'a}rm{\'a}n constant and
-!  the index `1' indicates values at the center of the first
-!  grid box at the bottom (version 1). Another expression for $r$ can be
-!  derived using the mean value of the velocity in the lowest
-!  grid box, and not its value in the middle of the box (version 2). Also
-!  this method is supported in {\tt friction()} and can be activated by
-!  uncommenting one line in the code.
-!
-!  If no breaking surface waves are considered, the law of the wall
-!  also holds at the surface. The surface roughness length may
-!  be calculated according to the \cite{Charnock55} formula,
-!  \begin{equation}
-!   \label{Charnock}
-!    z_0^s=\alpha \frac{(u_*^s)^2}{g}
-!   \point
-!  \end{equation}
-!  The model constant $\alpha$ is read in as {\tt charnock\_val} from
-!  the {\tt meanflow} namelist.
-!  If lake is true, this module calculates the bottom friction at every grid
-!  cell.
 !
 ! !USES:
    use meanflow,      only: int_flows,int_fwf,lake,Af
