332be5b2259da1d63aff6d5e416fbc2df0d66f22
Karsten Bolding <bold@dmu.dk>
2013-10-31 15:09:23 +0100

    proper treatment of observed sst and sss

--- src/airsea/airsea.F90
+++ src/airsea/airsea.F90
@@ -465,24 +465,6 @@
          case default
       end select
 
-!     The sea surface temperature
-      select case (sst_method)
-         case (FROMFILE)
-            call register_input_0d(sst_file,1,sst_obs)
-            LEVEL2 'Reading sea surface temperature from:'
-            LEVEL3 trim(sst_file)
-         case default
-      end select
-
-!     The sea surface salinity
-      select case (sss_method)
-         case (FROMFILE)
-            call register_input_0d(sss_file,1,sss)
-            LEVEL2 'Reading sea surface salinity from:'
-            LEVEL3 trim(sss_file)
-         case default
-      end select
-
    end if
 
 !  The fresh water fluxes (used for calc_fluxes=.true. and calc_fluxes=.false.)
@@ -501,6 +483,24 @@
          end if
          LEVEL2 'rain_impact=      ',rain_impact
          LEVEL2 'calc_evaporation= ',calc_evaporation
+      case default
+   end select
+
+!  The observed sea surface temperature
+   select case (sst_method)
+      case (FROMFILE)
+         call register_input_0d(sst_file,1,sst_obs)
+         LEVEL2 'Reading sea surface temperature from:'
+         LEVEL3 trim(sst_file)
+      case default
+   end select
+
+!  The observed sea surface salinity
+   select case (sss_method)
+      case (FROMFILE)
+         call register_input_0d(sss_file,1,sss)
+         LEVEL2 'Reading sea surface salinity from:'
+         LEVEL3 trim(sss_file)
       case default
    end select
 
@@ -562,10 +562,11 @@
          ty = const_ty*bio_drag_scale
       end if
 
-!     If reading SST from file, overwrite current (model) SST with observed value,
-!     to be used in output.
-      if (sst_method==FROMFILE) sst = sst_obs
    end if
+
+!  If reading SST from file, overwrite current (model) SST with observed value,
+!  to be used in output.
+   if (sst_method==FROMFILE) sst = sst_obs
 
 #ifndef INTERPOLATE_METEO   
    if (init_saved_vars) init_saved_vars = .false.
