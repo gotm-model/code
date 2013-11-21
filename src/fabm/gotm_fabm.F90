a00fcc6950212168844749850c8e675b00553ce7
Jorn Bruggeman <jorn@bolding-burchard.com>
2013-11-21 13:56:38 +0000

    fabm: renamed vertical_mean to avoid ambiguity

--- src/fabm/gotm_fabm.F90
+++ src/fabm/gotm_fabm.F90
@@ -761,14 +761,14 @@
       do while (associated(expression))
          select type (expression)
             class is (type_vertical_mean)
-               expression%out%p = vertical_mean(expression)
+               expression%out%p = calculate_vertical_mean(expression)
          end select
          expression => expression%next
       end do
       
    contains
    
-      REALTYPE function vertical_mean(expression)
+      REALTYPE function calculate_vertical_mean(expression)
          class (type_vertical_mean),intent(in) :: expression
 
          ! Loop over all levels, surface to bottom, and compute vertical mean.
@@ -795,7 +795,7 @@
             end if
          end do
          weights = weights/(min(expression%maximum_depth,depth)-expression%minimum_depth)
-         vertical_mean = sum(expression%in%p(1:nlev)*weights)
+         calculate_vertical_mean = sum(expression%in%p(1:nlev)*weights)
       end function
 
    end subroutine
