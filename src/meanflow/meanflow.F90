010266e3d98182e6d5a7b54c9df5063d9199b113
Lars Umlauf <lars.umlauf@io-warnemuende.de>
2014-05-06 14:22:45 +0200

    removed bug in deallocation of EXTRA_OUTPUT

--- src/meanflow/meanflow.F90
+++ src/meanflow/meanflow.F90
@@ -420,11 +420,11 @@
    if (allocated(avh)) deallocate(avh)
    if (allocated(bioshade)) deallocate(bioshade)
 # ifdef EXTRA_OUTPUT
-   if (allocated(mean1)) dallocate(mean1)
-   if (allocated(mean2)) dallocate(mean2)
-   if (allocated(mean3)) dallocate(mean3)
-   if (allocated(mean4)) dallocate(mean4)
-   if (allocated(mean5)) dallocate(mean5)
+   if (allocated(mean1)) deallocate(mean1)
+   if (allocated(mean2)) deallocate(mean2)
+   if (allocated(mean3)) deallocate(mean3)
+   if (allocated(mean4)) deallocate(mean4)
+   if (allocated(mean5)) deallocate(mean5)
 # endif
    LEVEL2 'done.'
 
