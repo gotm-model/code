118fff06c2498f429849cb77db35274318d23965
Karsten Bolding <karsten@bolding-burchard.com>
2014-12-18 11:31:07 +0100

    allow for different Albedo formulations, separated out zenith angle calculation from short wave radiation and added Cogley albedo

--- src/airsea/short_wave_radiation.F90
+++ src/airsea/short_wave_radiation.F90
@@ -5,11 +5,11 @@
 ! !ROUTINE: Calculate the short--wave radiation \label{sec:swr}
 !
 ! !INTERFACE:
-   REALTYPE function short_wave_radiation(yday,hh,dlon,dlat,cloud)
+   REALTYPE function short_wave_radiation(zenith_angle,yday,dlon,dlat,cloud)
 !
 ! !DESCRIPTION:
 !  This subroutine calculates the short--wave net radiation based on
-!  latitude, longitude, time, fractional cloud cover.
+!  solar zenith angle, year day, longitude, latitude, and fractional cloud cover.
 !  No corrections for albedo - must be done by calls to albedo_water() and
 !  if ice is included albedo_ice().
 !  The basic formula for the short-wave radiation at the surface, $Q_s$,
@@ -31,13 +31,10 @@
    IMPLICIT NONE
 !
 ! !INPUT PARAMETERS:
+   REALTYPE, intent(in)                :: zenith_angle
    integer, intent(in)                 :: yday
-   REALTYPE, intent(in)                :: hh
    REALTYPE, intent(in)                :: dlon,dlat
    REALTYPE, intent(in)                :: cloud
-!
-! !OUTPUT PARAMETERS:
-!   REALTYPE                            :: swr
 !
 ! !REVISION HISTORY:
 !  Original author(s): Karsten Bolding & Hans Burchard
@@ -52,44 +49,32 @@
    REALTYPE, parameter       :: tau=0.7
    REALTYPE, parameter       :: aozone=0.09
 
-   REALTYPE                  :: th0,th02,th03,sundec
-   REALTYPE                  :: thsun,coszen,zen,dzen,sunbet
+   REALTYPE                  :: coszen,sunbet
    REALTYPE                  :: qatten,qzer,qdir,qdiff,qtot,qshort
    REALTYPE                  :: rlon,rlat,eqnx
    REALTYPE                  :: yrdays
 !EOP
 !-----------------------------------------------------------------------
 !BOC
-!  from now on everything in radians
-   rlon = deg2rad*dlon
-   rlat = deg2rad*dlat
-
-   yrdays=365.
-   eqnx = (yday-81.)/yrdays*2.*pi
-
-   th0 = 2.*pi*yday/yrdays
-   th02 = 2.*th0
-   th03 = 3.*th0
-!  sun declination :
-   sundec = 0.006918 - 0.399912*cos(th0) + 0.070257*sin(th0)         &
-           - 0.006758*cos(th02) + 0.000907*sin(th02)                 &
-           - 0.002697*cos(th03) + 0.001480*sin(th03)
-!  sun hour angle :
-   thsun = (hh-12.)*15.*deg2rad + rlon
-
-!  cosine of the solar zenith angle :
-   coszen =sin(rlat)*sin(sundec)+cos(rlat)*cos(sundec)*cos(thsun)
+   coszen = cos(deg2rad*zenith_angle)
    if (coszen .le. 0.0) then
       coszen = 0.0
       qatten = 0.0
    else
       qatten = tau**(_ONE_/coszen)
    end if
+
    qzer  = coszen * solar
    qdir  = qzer * qatten
    qdiff = ((_ONE_-aozone)*qzer - qdir) * 0.5
    qtot  =  qdir + qdiff
 
+!  from now on everything in radians
+   rlon = deg2rad*dlon
+   rlat = deg2rad*dlat
+
+   yrdays=365.
+   eqnx = (yday-81.)/yrdays*2.*pi
 !  sin of the solar noon altitude in radians :
    sunbet=sin(rlat)*sin(eclips*sin(eqnx))+cos(rlat)*cos(eclips*sin(eqnx))
 !  solar noon altitude in degrees :
@@ -112,7 +97,6 @@
       qshort  = qtot*(1-0.62*cloud + 0.0019*sunbet)
    endif
 #endif
-
    short_wave_radiation = qshort
 
    return
