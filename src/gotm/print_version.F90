25b79677ddb172717c8d9df159527d5254bfa23a
Karsten Bolding <karsten@bolding-bruggeman.com>
2016-03-31 14:53:58 +0200

    forgot this one print_version.F90

---
+++ src/gotm/print_version.F90
@@ -0,0 +1,41 @@
+#include "cppdefs.h"
+!-----------------------------------------------------------------------
+!BOP
+!
+! !ROUTINE: print_version() - prints GOTM and optional FABM versions
+!
+! !INTERFACE:
+   subroutine print_version()
+!
+! !DESCRIPTION:
+!  Use git to obtain latest git hashes for GOTM and FABM.
+!  Also print compiler information from GOTM.
+!
+! !USES:
+   use gotm_version, only: gotm_commit_id=>git_commit_id, &
+                           gotm_branch_name=>git_branch_name
+   use gotm_compilation
+#ifdef _FABM_
+   use fabm_version, only: fabm_commit_id=>git_commit_id, &
+                           fabm_branch_name=>git_branch_name
+#endif
+   IMPLICIT NONE
+!
+!EOP
+!-----------------------------------------------------------------------
+!BOC
+   LEVEL0 LINE
+   LEVEL0 'GOTM version: ',gotm_commit_id,' (',gotm_branch_name,' branch)'
+#ifdef _FABM_
+   LEVEL0 'FABM version: ',fabm_commit_id,' (',fabm_branch_name,' branch)'
+#endif
+   LEVEL0 LINE
+   LEVEL0 'Compiler: ',compiler_id,' ',compiler_version
+   return
+   end subroutine print_version
+!EOC
+
+!-----------------------------------------------------------------------
+! Copyright (C) 2016 - Karsten Bolding (BB)                            !
+!-----------------------------------------------------------------------
+
