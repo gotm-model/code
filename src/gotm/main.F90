7093e803dce99d60a9c5635219115347318f30e1
Karsten Bolding <karsten@bolding-bruggeman.com>
2016-03-31 14:17:29 +0200

    print_version() in separate file

--- src/gotm/main.F90
+++ src/gotm/main.F90
@@ -115,6 +115,10 @@
       case ('-v', '--version')
          call print_version()
          stop
+      case ('-c', '--compile')
+         call print_version()
+         call compilation_options()
+         stop
       case ('-h', '--help')
          call print_help()
          stop
@@ -128,34 +132,7 @@
 
    end subroutine  cmdline
 
-   subroutine print_version()
-      use gotm_version
-      use gotm_compilation
-      use fabm_version,fabm_commit_id=>git_commit_id,fabm_branch_name=>git_branch_name
-
-      STDERR LINE
-      STDERR 'GOTM version: ',git_commit_id,' (',git_branch_name,' branch)'
-#ifdef _FABM_
-      STDERR 'FABM version: ',fabm_commit_id,' (',fabm_branch_name,' branch)'
-#endif
-      STDERR LINE
-      STDERR 'Compiler: ',compiler_id,' ',compiler_version
-      STDERR 'Compilation options: '
-!
-#ifndef GFORTRAN
-!   STDERR compiler_version()
-!   STDERR compiler_options()
-#else
-#ifdef FORTRAN90
-      LEVEL1 'Fortran 90 compilation'
-#endif
-#ifdef FORTRAN95
-      LEVEL1 'Fortran 95 compilation'
-#endif
-#ifdef FORTRAN2003
-      LEVEL1 'Fortran 2003 compilation'
-#endif
-#endif
+   subroutine compilation_options()
 #ifdef _FABM_
       LEVEL1 '_FABM_'
 #endif
@@ -171,9 +148,8 @@
 #ifdef _FLEXIBLE_OUTPUT_
       LEVEL1 '_FLEXIBLE_OUTPUT_'
 #endif
-
       STDERR LINE
-   end subroutine print_version
+   end subroutine compilation_options
 
    subroutine print_help()
       print '(a)', 'Usage: gotm [OPTIONS]'
@@ -182,6 +158,7 @@
       print '(a)', ''
       print '(a)', '  -h, --help        print usage information and exit'
       print '(a)', '  -v, --version     print version information'
+      print '(a)', '  -c, --compiler    print compilation options'
       print '(a)', ''
    end subroutine print_help
 
