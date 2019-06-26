d8f4befbfbd3c9451881c18503f61f5317006ff8
Karsten Bolding <karsten@bolding-bruggeman.com>
2019-06-26 11:45:14 +0200

    added YAML and flexout version info

--- src/gotm/print_version.F90
+++ src/gotm/print_version.F90
@@ -15,6 +15,10 @@
    use gotm_version, only: gotm_commit_id=>git_commit_id, &
                            gotm_branch_name=>git_branch_name
    use gotm_compilation
+   use yaml_version, only: yaml_commit_id=>git_commit_id, &
+                           yaml_branch_name=>git_branch_name
+   use flexout_version, only: flexout_commit_id=>git_commit_id, &
+                              flexout_branch_name=>git_branch_name
 #ifdef _FABM_
    use fabm, only: fabm_initialize_library
    use fabm_types, only: type_version,first_module_version
@@ -34,18 +38,20 @@
 !-----------------------------------------------------------------------
 !BOC
    LEVEL0 LINE
-   LEVEL0 'GOTM version:   ',gotm_commit_id,' (',gotm_branch_name,' branch)'
+   LEVEL0 'GOTM:    ',gotm_commit_id,' (',gotm_branch_name,' branch)'
+   LEVEL0 'YAML:    ',yaml_commit_id,' (',yaml_branch_name,' branch)'
+   LEVEL0 'flexout: ',flexout_commit_id,' (',flexout_branch_name,' branch)'
 #ifdef _FABM_
-   LEVEL0 'FABM version:   ',fabm_commit_id,' (',fabm_branch_name,' branch)'
+   LEVEL0 'FABM:    ',fabm_commit_id,' (',fabm_branch_name,' branch)'
    call fabm_initialize_library()
    version => first_module_version
    do while (associated(version))
-      LEVEL0 trim(version%module_name)//' version:   ',trim(version%version_string)
+      LEVEL0 trim(version%module_name)//':  ',trim(version%version_string)
       version => version%next
    end do
 #endif
 #ifdef NETCDF_FMT
-   LEVEL0 'NetCDF version: ',trim(NF90_INQ_LIBVERS())
+   LEVEL0 'NetCDF: ',trim(NF90_INQ_LIBVERS())
 #endif
    LEVEL0 LINE
    LEVEL0 'Compiler: ',compiler_id,' ',compiler_version
