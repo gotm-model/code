4c1428306cf8d1813faa48f23920ea807e2bf88a
Karsten Bolding <karsten@bolding-bruggeman.com>
2020-07-02 14:47:18 +0200

    run entrainment tests

--- tests/entrainment/run_all.sh
+++ tests/entrainment/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 confs="generic kepsilon komega MellorYamada"
-GOTM=/home/kb/local/gcc/8/gotm/5.3/bin/gotm
+GOTM=./gotm
 
 for conf in $confs; do
    $GOTM ${conf}_gotm.yaml --output_id _$conf >& $conf.log
