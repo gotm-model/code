4d537d1a275c17ce5601e941fd2c70e64cca3b95
Karsten Bolding <karsten@bolding-bruggeman.com>
2021-10-06 12:08:59 +0200

    use TEOS-10 in density calculations

--- src/util/equation_of_state.F90
+++ src/util/equation_of_state.F90
@@ -1,3 +1,5 @@
+!KB gsw_p_from_z
+!KB gsw_stabilise_SA_CT
 #include"cppdefs.h"
 !-----------------------------------------------------------------------
 !BOP
@@ -47,7 +49,9 @@
 !  \end{enumerate}
 
 !USES:
-   use gsw_mod_toolbox, only: gsw_rho
+   use settings
+   use gsw_mod_toolbox, only: gsw_rho,gsw_rho_alpha_beta
+!   use gsw_mod_toolbox, only: gsw_rho_alpha_beta_bsq
    IMPLICIT NONE
 
 !  default: all is private.
@@ -55,18 +59,18 @@
 !
 ! !PUBLIC MEMBER FUNCTIONS:
    public config_equation_of_state,eqstate1
-   integer, public           :: eq_state_method, eq_state_mode
-   REALTYPE, public          :: T0,S0,p0,dtr0,dsr0
+   integer, public :: eq_state_method
 !
 ! !REVISION HISTORY:
 !  Original author(s): Hans Burchard & Karsten Bolding
 !
 !  From JMFWG-06
-   REALTYPE, external        :: rho_from_theta
+!KB   REALTYPE, external        :: rho_from_theta
 !
 ! !LOCAL VARIABLES
-   logical                   :: press
-   REALTYPE                  :: rho0, rho_ref
+!KB   logical                   :: press
+   REALTYPE                  :: T0,S0,p0,rho0,dtr0,dsr0
+   REALTYPE                  :: rho_ref
 !EOP
 !-----------------------------------------------------------------------
 
@@ -92,71 +96,23 @@
 !  Original author(s): Karsten Bolding & Jorn Bruggeman
 !
 ! !LOCAL VARIABLES:
-   REALTYPE                  :: dTT,dSS,dum
+   integer, parameter :: rk = kind(_ONE_)
+   class (type_settings), pointer :: branch,twig
+   integer method
 !EOP
-!KB<<<<<<< HEAD:src/util/equation_of_state.F90
-#if 1
 !-----------------------------------------------------------------------
 !BOC
    rho_ref = rho_0
    rho0 = rho_0
 
-   select case (eq_state_mode)
-      case(1)
-         select case (eq_state_method)
-            case (1)
-               press=.true.
-            case (2)
-               press=.false.
-            case (3)
-               press=.true.   ! This allows for choosing potentials other than p=0
-               dTT=0.001
-               dSS=0.001
-               rho0= unesco(S0,T0,p0/10.d0,press)
-               dtr0=(unesco(S0,T0+0.5*dTT,p0/10.d0,press)        &
-                    -unesco(S0,T0-0.5*dTT,p0/10.d0,press))/dTT
-               dsr0=(unesco(S0+0.5*dSS,T0,p0/10.d0,press)        &
-                    -unesco(S0-0.5*dSS,T0,p0/10.d0,press))/dSS
-            case (4)
-            case default
-         end select
-      case(2)
-         select case (eq_state_method)
-            case (1)
-            case (2)
-            case (3)
-               call eosall_from_theta(S0,T0,p0,rho0,dsr0,dtr0,dum)
-            case (4)
-            case default
-         end select
-      case(3)
-         select case (eq_state_method)
-            case (1)
-            case (2)
-            case (3)
-               press=.true.   ! This allows for choosing potentials other than p=0
-               call gsw_rho_alpha_beta_bsq(S0,T0,p0,rho0,dtr0,dsr0); dtr0=-dtr0
-            case (4)
-            case default
-         end select
-      case default
-   end select
-   end subroutine config_equation_of_state
-#else
-!KB=======
-!
-! !LOCAL VARIABLES:
-   integer, parameter :: rk = kind(_ONE_)
-   class (type_settings), pointer :: twig
-!
-!-----------------------------------------------------------------------
-!BOC
    LEVEL1 'init_eqstate_yaml'
-   call branch%get(eq_state_mode, 'method', 'method', default=2, &
-                   options=(/option(1, 'UNESCO', 'UNESCO'), option(2, 'Jackett et al. (2005)', 'Jackett')/))
-   call branch%get(eq_state_method, 'form', 'formulation', &
-                   options=(/option(1, 'full', 'full'), option(2, 'full with buoyancy frequency based on surface pressure', 'full-pot'), &
-                   option(3, 'linearized at T0,S0,p0', 'linear'), option(4, 'linearized at T0,S0,p0,dtr0,dsr0', 'linear_custom')/), default=1)
+   
+   branch => settings_store%get_child('eq_state', 'equation of state')
+   call branch%get(method, 'method', 'density formulation', &
+                   options=(/option(1, 'linearized at T0, S0, p0', 'linear'), &
+                             option(2, 'linearized at T0, S0, rho0, dtr0, dsr0', 'linear_custom'), &
+                             option(3, 'TEOS-10', 'TEOS-10')/), default=3)
+   eq_state_method=method
    twig => branch%get_child('linear')
    call twig%get(T0, 'T0', 'reference temperature', 'Celsius', &
                  minimum=-2._rk, default=10._rk)
@@ -164,16 +120,39 @@
                  minimum=0._rk, default=35._rk)
    call twig%get(p0, 'p0', 'reference pressure', 'Pa', &
                  default=0._rk)
+   call twig%get(rho0, 'rho0', 'reference density', 'kg/m3', &
+                 default=0._rk)
    call twig%get(dtr0, 'dtr0', 'thermal expansion coefficient', 'kg/m^3/K', &
                  default=-0.17_rk)
    call twig%get(dsr0, 'dsr0', 'saline expansion coefficient', 'kg/m^3/psu', &
                  default=0.78_rk)
 
-   init_linearised = .true.
-   LEVEL2 'done.'
-   end subroutine init_eqstate_yaml
+   select case (eq_state_method)
+      case(1) ! S0, T0, p0 are provided - rho0, dtr0, dsr0 are calculated
+#if 1
+         call gsw_rho_alpha_beta(S0,T0,p0,rho0,dtr0,dsr0)
+#else
+         call gsw_rho_alpha_beta_bsq(S0,T0,p0,rho0,dtr0,dsr0,.true.)
 #endif
-!KB>>>>>>> origin/master:src/util/eqstate.F90
+         LEVEL2 'Linearized - using gsw_rho_alpha_beta()'
+         LEVEL3 'S0=    ',S0
+         LEVEL3 'T0=    ',T0
+         LEVEL3 'p0=    ',p0
+         LEVEL3 'rho0=  ',rho0
+         LEVEL3 'alpha= ',dtr0
+         LEVEL3 'beta=  ',dsr0
+      case(2) ! S0, T0, rho0, alpha, beta are all provided
+         LEVEL2 'Linearized - custom - using S0, T0, rho0, alpha, beta'
+         LEVEL3 'S0=    ',S0
+         LEVEL3 'T0=    ',T0
+         LEVEL3 'rho0=  ',rho0
+         LEVEL3 'alpha= ',dtr0
+         LEVEL3 'beta=  ',dsr0
+      case(3) ! use gsw_rho(S,T,p) - default p=0
+      case default
+   end select
+   LEVEL2 'done.'
+   end subroutine config_equation_of_state
 !EOC
 
 !-----------------------------------------------------------------------
@@ -183,7 +162,7 @@
 !
 ! !INTERFACE:
 !KB   elemental REALTYPE function eqstate1(S,T,p,g)
-   REALTYPE function eqstate1(S,T,p,g)
+   elemental REALTYPE function eqstate1(S,T,p,g)
 !
 ! !DESCRIPTION:
 !  Calculates the in-situ buoyancy according to the selected method.
@@ -232,34 +211,15 @@
 !  Original author(s): Hans Burchard & Karsten Bolding
 !
 ! !LOCAL VARIABLES:
-   REALTYPE                  :: x,dum
-   REALTYPE                  :: dTT,dSS
+   REALTYPE :: x
 !EOP
 !-----------------------------------------------------------------------
 !BOC
    select case (eq_state_method)
-      case (1)
-         select case (eq_state_mode)
-            case (1)
-               x=unesco(S,T,p/10.,press)
-            case (2)
-               x=rho_from_theta(S,T,p)
-            case (3)
-               x=gsw_rho(S,T,p)
-            case default
-         end select
-      case(2)
-         select case (eq_state_mode)
-            case (1)
-               x=unesco(S,T,p,press)
-            case (2)
-               x=rho_from_theta(S,T,0.d0)
-            case (3)
-               x=gsw_rho(S,T,0.d0)
-            case default
-         end select
-      case (3,4)
+      case (1, 2)
          x=rho0+dtr0*(T-T0)+dsr0*(S-S0)
+      case(3)
+         x=gsw_rho(S,T,p0)
       case default
    end select
 
@@ -274,149 +234,7 @@
    end function eqstate1
 !EOC
 
-#if 0
 !-----------------------------------------------------------------------
-!BOP
-!
-! !IROUTINE: Compute thermal expansion coefficient
-!
-! !INTERFACE:
-   REALTYPE function eos_alpha(S,T,p,g,rho_0)
-!
-! !DESCRIPTION:
-!  Computes the thermal expansion coefficient defined by
-!  \begin{equation}
-!   \label{eosAlpha}
-!        \alpha =
-!         - \dfrac{1}{\rho_0}
-!        \left( \partder{\rho_{is}}{T} \right)_S
-!	 =
-!        \dfrac{1}{g}
-!        \left( \partder{B_{is}}{T} \right)_S
-!        \comma
-!  \end{equation}
-!  where $B_{is}$ denotes the in-situ buoyancy. The computation is carried
-!  out by a finite difference approximation of \eq{eosAlpha},
-!  requiring two evaluations of the equation of state.
-!  Note, that comparing \eq{eosAlpha} with \eq{eosLinear} it follows that
-!  $\alpha = - \text{\tt dtr0}/\rho_0$.
-!
-!  Thic function is now obsolete as we use functionality from gsw_toolbox:
-!  gsw_rho_alpha_beta_bsq()
-!EOP
-!-----------------------------------------------------------------------
-!BOC
-   end function eos_alpha
-!EOC
-
-!-----------------------------------------------------------------------
-!BOP
-!
-! !IROUTINE: Compute saline contraction coefficient
-!
-! !INTERFACE:
-   REALTYPE function eos_beta(S,T,p,g,rho_0)
-!
-! !DESCRIPTION:
-!  Computes the saline contractioncoefficient defined by
-!  \begin{equation}
-!   \label{eosBeta}
-!        \beta =
-!         \dfrac{1}{\rho_0}
-!        \left( \partder{\rho_{is}}{S} \right)_T
-!	 =
-!        - \dfrac{1}{g}
-!        \left( \partder{B_{is}}{S} \right)_T
-!        \comma
-!  \end{equation}
-!  where $B_{is}$ denotes the in-situ buoyancy. The computation is carried
-!  out by a finite difference approximation of \eq{eosBeta},
-!  requiring two evaluations of the equation of state.
-!  Note, that comparing \eq{eosBeta} with \eq{eosLinear} it follows that
-!  $\beta = \text{\tt dsr0}/\rho_0$.
-!
-!  Thic function is now obsolete as we use functionality from gsw_toolbox.
-!  gsw_rho_alpha_beta_bsq()
-!-----------------------------------------------------------------------
-!BOC
-   end function eos_beta
-!EOC
-#endif
-
-!-----------------------------------------------------------------------
-!BOP
-!
-! !IROUTINE: The UNESCO equation of state
-!
-! !INTERFACE:
-   REALTYPE function unesco(S,T,p,UNPress)
-!
-! !DESCRIPTION:
-!  Computes the in-situ density in \eq{DefEOS} according to the
-!  UNESCO equation of state for sea water (see \cite{FofonoffMillard83}).
-!  The pressure
-!  dependence can be switched on ({\tt UNPress=.true.}) or off
-!  ({\tt UNPress=.false.}). {\tt S} is salinity $S$ in psu, {\tt T} is
-!  potential temperature $\theta$ in $^{\circ}$C (ITS-90), {\tt p} is
-!  gauge pressure (absolute pressure - 10.1325 bar) and
-!  {\tt  unesco} is the in-situ density in kg\,m$^{-3}$.
-!  The check value is {\tt unesco(35,25,1000) = 1062.53817} .
-!
-! !USES:
-   IMPLICIT NONE
-!
-! !INPUT PARAMETERS:
-   REALTYPE, intent(in)                :: S,T,p
-   LOGICAL, intent(in)                 :: UNPress
-!
-! !REVISION HISTORY:
-!  Original author(s): Hans Burchard & Karsten Bolding
-!
-!EOP
-!
-! !LOCAL VARIABLES:
-   REALTYPE                  :: x,K
-   REALTYPE                  :: T2,T3,T4,T5,S15,S2,S3,p2
-!-----------------------------------------------------------------------
-!BOC
-   T2 = T*T
-   T3 = T*T2
-   T4 = T2*T2
-   T5 = T*T4
-   S15= S**1.5
-   S2 = S*S
-   S3 = S*S2
-
-   x=999.842594+6.793952e-02*T-9.09529e-03*T2+1.001685e-04*T3
-   x=x-1.120083e-06*T4+6.536332e-09*T5
-   x=x+S*(0.824493-4.0899e-03*T+7.6438e-05*T2-8.2467e-07*T3)
-   x=x+S*5.3875e-09*T4
-   x=x+sqrt(S3)*(-5.72466e-03+1.0227e-04*T-1.6546e-06*T2)
-   x=x+4.8314e-04*S2
-
-     if ((UNPress).and.(p.gt.0)) then
-     p2=p*p
-     K= 19652.21                                         &
-       +148.4206     *T          -2.327105    *T2        &
-       +  1.360477E-2*T3         -5.155288E-5 *T4        &
-       +  3.239908      *p       +1.43713E-3  *T *p      &
-       +  1.16092E-4 *T2*p       -5.77905E-7  *T3*p      &
-       +  8.50935E-5    *p2      -6.12293E-6  *T *p2     &
-       +  5.2787E-8  *T2*p2                              &
-       + 54.6746             *S  -0.603459    *T    *S   &
-       +  1.09987E-2 *T2     *S  -6.1670E-5   *T3   *S   &
-       +  7.944E-2           *S15+1.6483E-2   *T    *S15 &
-       -  5.3009E-4  *T2     *S15+2.2838E-3      *p *S   &
-       -  1.0981E-5  *T *p   *S  -1.6078E-6   *T2*p *S   &
-       +  1.91075E-4    *p   *S15-9.9348E-7      *p2*S   &
-       +  2.0816E-8  *T *p2*S    +9.1697E-10  *T2*p2*S
-     x=x/(1.-p/K)
-   end if
-
-   unesco=x
-   return
-   end function unesco
-!EOC
 
    end module eqstate
 
