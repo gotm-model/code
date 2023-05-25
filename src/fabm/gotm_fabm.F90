c6f5b1f0ec0a358fd557f920147c31a37b2b4c53
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-05-25 10:44:03 +0100

    use explicit unit for fabm log messages

--- src/fabm/gotm_fabm.F90
+++ src/fabm/gotm_fabm.F90
@@ -1769,7 +1769,7 @@
 !EOP
 !-----------------------------------------------------------------------!
 !BOC
-   write (*,*) trim(message)
+   STDOUT trim(message)
    end subroutine gotm_driver_log_message
 !EOC
 
