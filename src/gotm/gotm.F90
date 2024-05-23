gotm-model/code

commit 459c09b3063775c4694b09d46987c26b87650397

Pass u_taub to cvmix

src/gotm/gotm.F90
@@ -255,7 +255,7 @@
                    default='2017-01-01 00:00:00')
    call branch%get(stop, 'stop', 'stop date and time', units='yyyy-mm-dd HH:MM:SS', &
                    default='2018-01-01 00:00:00')
-   if (generate_restart_file) stop = start 
+   if (generate_restart_file) stop = start
    call branch%get(dt, 'dt', 'time step for integration', 's', &
                    minimum=0.e-10_rk, default=3600._rk)
    call branch%get(cnpar, 'cnpar', '"implicitness" of diffusion scheme', '1', &
@@ -294,7 +294,7 @@
    call twig%get(dtgrid, 'dtgrid', 'time step (must be fraction of dt)', 's', &
                  minimum=0._rk,default=5._rk)
 #endif
-   
+
 
    ! Predefine order of top-level categories in gotm.yaml
    branch => settings_store%get_child('temperature')
@@ -373,7 +373,7 @@
                  default=0.00075_rk)
    call twig%get(cp, 'cp', 'specific heat capacity', 'J/(kg/K)', &
                  minimum=0._rk,default=3991.86796_rk)
-   
+
 
    LEVEL1 'init_density()'
    call init_density(nlev)
@@ -746,7 +746,7 @@
             call set_ssuv(u(nlev),v(nlev))
 #ifdef _ICE_
          else
-            call set_sst(Tice_surface) !GSW_KB - check for GSW alternative 
+            call set_sst(Tice_surface) !GSW_KB - check for GSW alternative
             call set_ssuv(_ZERO_,_ZERO_)
 #endif
          end if
@@ -839,7 +839,7 @@
 !  update shear and stratification
    call shear(nlev,cnpar)
    call do_density(nlev,S,T,-z,-zi)
-   buoy(1:nlev) = -gravity*(rho_p(1:nlev)-rho0)/rho0  
+   buoy(1:nlev) = -gravity*(rho_p(1:nlev)-rho0)/rho0
    call stratification(nlev)
 
 #ifdef SPM
@@ -863,7 +863,7 @@
 
          ! update Langmuir number
          call langmuir_number(nlev,zi,Hs_input%value,u_taus,zi(nlev)-zsbl,u10_input%value,v10_input%value)
-         
+
          select case(kpp_langmuir_method)
          case (0)
             efactor = _ONE_
@@ -879,9 +879,9 @@
             La = La_SLP_RWH16
          end select
 
-         !  update turbulence parameter from CVMIX 
-         call do_cvmix(nlev,depth,h,rho_p,u,v,NN,NNT,NNS,SS,            &
-                       u_taus,tFlux,btFlux,sFlux,bsFlux,                &
+         !  update turbulence parameter from CVMix
+         call do_cvmix(nlev,depth,h,rho,u,v,NN,NNT,NNS,SS,              &
+                       u_taus,u_taub,tFlux,btFlux,sFlux,bsFlux,         &
                        tRad,bRad,cori,efactor,La)
 #endif
 

src/cvmix/gotm_cvmix.F90
@@ -729,7 +729,7 @@
 ! !IROUTINE: Do KPP with CVMix
 !
 ! !INTERFACE:
-   subroutine do_cvmix(nlev,h0,h,rho,u,v,NN,NNT,NNS,SS,u_taus,         &
+   subroutine do_cvmix(nlev,h0,h,rho,u,v,NN,NNT,NNS,SS,u_taus,u_taub,  &
                        tFlux,btFlux,sFlux,bsFlux,tRad,bRad,f,          &
                        EFactor,LaSL)
 !
@@ -765,8 +765,8 @@
 !  square of shear frequency (1/s^2)
    REALTYPE, intent(in)                          :: SS(0:nlev)
 
-!  surface friction velocities (m/s)
-   REALTYPE, intent(in)                          :: u_taus
+!  surface and bottom friction velocities (m/s)
+   REALTYPE, intent(in)                          :: u_taus,u_taub
 
 !  surface temperature flux (K m/s) and
 !  salinity flux (psu m/s) (negative for loss)

commit 3f59decce4bdf82f925a1bded0420f3785ad656c

Use potential density in CVMix -- bug introduced in 459c09b3

src/gotm/gotm.F90
@@ -880,7 +880,7 @@
          end select
 
          !  update turbulence parameter from CVMix
-         call do_cvmix(nlev,depth,h,rho,u,v,NN,NNT,NNS,SS,              &
+         call do_cvmix(nlev,depth,h,rho_p,u,v,NN,NNT,NNS,SS,            &
                        u_taus,u_taub,tFlux,btFlux,sFlux,bsFlux,         &
                        tRad,bRad,cori,efactor,La)
 #endif